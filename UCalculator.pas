b07233a8e231f89ad65d294e19ab11cb97fe134c
Renan Santos Gomes <renanonline8@gmail.com>
2019-02-27 22:56:04 -0300

    Worked in article Putting the Calculator Together
    Done subtitle Aplication Logic

--- UCalculator.pas
+++ UCalculator.pas
@@ -11,6 +11,24 @@ type
     class function Divide(a, b: Double): Double;
   end;
 
+  TOperatorEnum = (opNull, opAdd, opSubtract, opMultiply, opDivide);
+
+  TCalcStatus = class
+  private
+    FInputValue: string;
+    FCurrentTotal: Double;
+    FOperation: TOperatorEnum;
+    FDisplayTotal: Boolean;
+  public
+    constructor Create;
+
+    procedure AddDigit(Digit: string);
+    procedure AddDecimalSeparator;
+    procedure CalcTotal;
+    procedure NewOperation(Oper: TOperatorEnum);
+    function DisplayValue: string;
+  end;
+
 implementation
 
 {TCalculator}
@@ -33,6 +51,50 @@ end;
 class function TCalculator.Subtract(a, b: Double): Double;
 begin
   Result := a - b;
+end;
+
+procedure TCalcStatus.AddDecimalSeparator;
+begin
+  FInputValue := FInputValue +
+    FormatSettings.DecimalSeparator;
+end;
+
+procedure TCalcStatus.AddDigit(Digit: string);
+begin
+  FDisplayTotal := Digit;
+end;
+
+procedure TCalcStatus.NewOperation(Oper: TOperatorEnum);
+begin
+  CalcTotal;
+  FOperation := Oper;
+end;
+
+procedure TCalcStatus.CalcTotal;
+var
+  NewValue: Double;
+begin
+  NewValue := StrToFloatDef(FInputValue, 0);
+  case FOperation of
+    opNull: FCurrentTotal := NewValue;
+    opAdd: FCurrentTotal := TCalculator.Add (FCurrentTotal, NewValue);
+    opSubtract: FCurrentTotal := TCalculator.Subtract (FCurrentTotal, NewValue);
+    opMultiply: FCurrentTotal := TCalculator.Multiply (FCurrentTotal, NewValue);
+    opDivide: FCurrentTotal := TCalculator.Divide (FCurrentTotal, NewValue);
+  end;
+
+  //reset status
+  FOperation := opNull;
+  FOperation := True;
+  FInputValue := '';
+end;
+
+function TCalcStatus.DisplayValue: string;
+begin
+  if FDisplayTotal then
+    Result := FloatToStr(FCurrentTotal);
+  else
+    Result := FinputValue;
 end;
 
 end.
--- uFormCalculator.pas
+++ uFormCalculator.pas
@@ -11,6 +11,23 @@ type
   TFormCalc = class(TForm)
     LayoutDisplay: TLayout;
     LabelDisplay: TLabel;
+    GridLayoutButtons: TGridLayout;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    Button10: TButton;
+    Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button16: TButton;
   private
     { Private declarations }
   public
